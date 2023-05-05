5901e411e0221c469e9498c7253769bb0ea96363
Kalybaev Rustam <me@kalybaev.ru>
2023-05-05 21:58:27 +0500

    task3 done

---
+++ task 3/Program.cs
@@ -0,0 +1,27 @@
+﻿int x = ReadInt("Введите число: ");
+System.Console.WriteLine(string.Join(" ", Cube(x)));
+
+int ReadInt(string argument)
+{
+  Console.Write(argument);
+
+  int i;
+
+  while (!int.TryParse(Console.ReadLine(), out i))
+  {
+    Console.WriteLine("It's not a number");
+  }
+
+  return i;
+}
+
+int [] Cube(int x)
+{
+        int [] Positive = new int [x];
+        for (int i = 0; i < x; i++)
+        {
+            Positive[i] = (int)Math.Pow(i + 1, 3); 
+        }
+    
+        return Positive;   
+}
